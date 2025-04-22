1a855b1b948c08717bcec2f6fdbcdadfab0b54d7
willyliang <willy_liang@ecomsoft.com.tw>
2025-04-22 17:51:21 +0800

    flux chat

--- src/main/java/com/test/springai/controller/AIController.java
+++ src/main/java/com/test/springai/controller/AIController.java
@@ -2,11 +2,12 @@ package com.test.springai.controller;
 
 import com.test.springai.service.AIService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +16,13 @@ public class AIController {
 
     private final AIService aiService;
 
-    @GetMapping
-    public String test(@RequestParam String topic) {
+    @GetMapping("/joke")
+    public String getJoke(@RequestParam String topic) {
         return aiService.getJoke(topic);
+    }
+
+    @GetMapping(value = "/stream/joke", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getJokeFlux(@RequestParam String topic) {
+        return aiService.getJokeFlux(topic);
     }
 }
--- src/main/java/com/test/springai/service/AIService.java
+++ src/main/java/com/test/springai/service/AIService.java
@@ -3,9 +3,8 @@ package com.test.springai.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 @Service
 @Slf4j
@@ -21,14 +20,32 @@ public class AIService {
                 請兩位給我講一個關於{topic}的段子
                 """;
 
-        ChatResponse response = chatClient.prompt()
+//        ChatResponse response = chatClient.prompt()
+//                .user(u -> u.text(prompt).param("topic", topic))
+//                .call()
+//                .chatResponse();
+//
+//        log.info("response: {}", response);
+//
+//        assert response != null;
+//        return response.getResult().getOutput().getText();
+
+        return chatClient.prompt()
                 .user(u -> u.text(prompt).param("topic", topic))
                 .call()
-                .chatResponse();
+                .content();
+    }
 
-        log.info("response: {}", response);
+    public Flux<String> getJokeFlux(String topic) {
+        String prompt = """
+                你將扮演兩位相聲演員，主要語言為台灣繁體中文，
+                其中一位嚴肅，其中一位講話瘋癲，
+                請兩位給我講一個關於{topic}的段子
+                """;
 
-        assert response != null;
-        return response.getResult().getOutput().getText();
+        return chatClient.prompt()
+                .user(u -> u.text(prompt).param("topic", topic))
+                .stream()
+                .content();
     }
 }
